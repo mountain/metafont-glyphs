143edd73a42edbf40731ed44a50440a057d90ec7
Mingli Yuan <mingli.yuan@gmail.com>
2023-01-28 17:24:45 +0800

    add shell params

--- clean-all.sh
+++ clean-all.sh
@@ -1,4 +1,4 @@
-ninja -f ../build/build.ninja -t clean
+ninja -f ./build/build.ninja -t clean
 cd ./temp && rm *.aux && rm *.tfm && cd ..
 cd ./metafont && rm *.mf && cd ..
 cd ./vector && rm *.csv && cd ..
