b12f29fd12d9873826571aaf37a03453c386cf11
Mingli Yuan <mingli.yuan@gmail.com>
2023-01-28 17:27:11 +0800

    fix shell dirs

--- clean-all.sh
+++ clean-all.sh
@@ -1,5 +1,4 @@
 cd temp && ninja -f ../build/build.ninja -t clean && cd ..
-cd temp && rm *.aux && rm *.tfm && cd ..
 cd metafont && rm *.mf && cd ..
 cd vector && rm *.csv && cd ..
 cd glyph && rm *.png && cd ..
