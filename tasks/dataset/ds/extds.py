38fd17639932e3ef84a6a0f5947222ecbd732067
Mingli Yuan <mingli.yuan@gmail.com>
2023-01-30 13:24:52 +0800

    glyph reconstruction verified

--- tasks/dataset/ds/extds.py
+++ tasks/dataset/ds/extds.py
@@ -4,3 +4,9 @@ import cv2
 import pyarrow as pa
 import pyarrow.parquet as pq
 
+
+table = pq.read_table("data/dataset/validation.parquet").to_pandas()
+for ix, glyph in enumerate(table['glyphs']):
+    img = glyph.reshape((96, 96))
+    cv2.imwrite("temp/%04d.png" % ix, img)
+
