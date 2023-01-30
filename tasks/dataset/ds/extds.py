3862228d5b9736a25fe9037f0de135403515e178
Mingli Yuan <mingli.yuan@gmail.com>
2023-01-30 13:58:39 +0800

    vectors reconstruction verified

--- tasks/dataset/ds/extds.py
+++ tasks/dataset/ds/extds.py
@@ -10,3 +10,16 @@ for ix, glyph in enumerate(table['glyphs']):
     img = glyph.reshape((96, 96))
     cv2.imwrite("temp/%04d.png" % ix, img)
 
+for ix, vector in enumerate(table['vectors']):
+    vec = vector.reshape((-1, 2))
+    with open("temp/%04d.csv" % ix, "w") as f:
+        for points in vec:
+            x, y = points
+            if x < 0 and y < 0:
+                f.write("%0.4f, %0.4f\n" % (x, y))
+            elif x < 0 <= y:
+                f.write("%0.4f, +%0.4f\n" % (x, y))
+            elif x >= 0 > y:
+                f.write("+%0.4f, %0.4f\n" % (x, y))
+            else:
+                f.write("+%0.4f, +%0.4f\n" % (x, y))
