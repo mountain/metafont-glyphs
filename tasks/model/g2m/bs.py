436b20e375b64c81de1629c4ee0f4c0df97011fc
Mingli Yuan <mingli.yuan@gmail.com>
2024-05-23 01:19:30 +0800

    fix hyperparams

--- tasks/model/g2m/bs.py
+++ tasks/model/g2m/bs.py
@@ -69,7 +69,7 @@ class Baseline(AbstractG2MNet):
         self.model_name = 'bs'
         self.vit = VisionTransformer(
             image_size=96, patch_size=12, num_layers=6, num_heads=16, num_classes=100,
-            hidden_dim=128, mlp_dim=256, dropout=0.1, attention_dropout=0.1
+            hidden_dim=128, mlp_dim=64, dropout=0.1, attention_dropout=0.1
         )
         first_conv = self.vit.conv_proj
         self.vit.conv_proj = nn.Conv2d(1, first_conv.out_channels,
