91637a443217d06e5d8398af63d801e7c05187b9
Mingli Yuan <mingli.yuan@gmail.com>
2024-05-22 23:30:43 +0800

    dataloader params

--- tasks/model/g2m/bs.py
+++ tasks/model/g2m/bs.py
@@ -51,13 +51,13 @@ class AbstractG2MNet(ltn.LightningModule):
         return lss
 
     def train_dataloader(self):
-        return DataLoader(ds.ParquetDataset("../../data/dataset/train.parquet"), batch_size=20, num_workers=8, shuffle=True)
+        return DataLoader(ds.ParquetDataset("../../data/dataset/train.parquet"), batch_size=128, num_workers=8, shuffle=True, drop_last=True, pin_memory=True, prefetch_factor=2, persistent_workers=True)
 
     def val_dataloader(self):
-        return DataLoader(ds.ParquetDataset("../../data/dataset/validation.parquet"), batch_size=10, num_workers=1, shuffle=False)
+        return DataLoader(ds.ParquetDataset("../../data/dataset/validation.parquet"), batch_size=64, num_workers=1, shuffle=False, drop_last=True, pin_memory=True, prefetch_factor=2, persistent_workers=True)
 
     def test_dataloader(self):
-        return DataLoader(ds.ParquetDataset("../../data/dataset/test.parquet"), batch_size=10, num_workers=1, shuffle=False)
+        return DataLoader(ds.ParquetDataset("../../data/dataset/test.parquet"), batch_size=64, num_workers=1, shuffle=False, drop_last=True, pin_memory=True, prefetch_factor=2, persistent_workers=True)
 
 
 class Baseline(AbstractG2MNet):
