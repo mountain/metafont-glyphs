18784ca0c90bbcb9c725fee552115b1fddd38ef5
Mingli Yuan <mingli.yuan@gmail.com>
2024-05-23 00:45:21 +0800

    fix hyperparam

--- tasks/model/g2m/bs.py
+++ tasks/model/g2m/bs.py
@@ -68,8 +68,8 @@ class Baseline(AbstractG2MNet):
         super().__init__()
         self.model_name = 'bs'
         self.vit = VisionTransformer(
-            image_size=96, patch_size=16, num_layers=12, num_heads=16, num_classes=100,
-            hidden_dim=256, mlp_dim=512, dropout=0.1, attention_dropout=0.1
+            image_size=96, patch_size=16, num_layers=6, num_heads=16, num_classes=100,
+            hidden_dim=512, mlp_dim=512, dropout=0.1, attention_dropout=0.1
         )
 
     def forward(self, glyph):
