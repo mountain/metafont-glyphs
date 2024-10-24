29c9ec5a29836a4bd978f620113d7479de668ca9
Mingli Yuan <mingli.yuan@gmail.com>
2024-10-25 01:34:51 +0800

    rm vit04

--- tasks/model/g2m/aeg.py
+++ tasks/model/g2m/aeg.py
@@ -102,10 +102,6 @@ class AEGModel(AbstractG2MNet):
         super().__init__()
         self.model_name = 'bs'
         self.num_layers = 4
-        self.vit08 = VisionTransformer(
-            image_size=96, patch_size=8, num_layers=self.num_layers, num_heads=16, num_classes=80,
-            hidden_dim=128, mlp_dim=512, dropout=0.1, attention_dropout=0.1
-        )
         self.vit16 = VisionTransformer(
             image_size=96, patch_size=16, num_layers=self.num_layers, num_heads=16, num_classes=80,
             hidden_dim=128, mlp_dim=512, dropout=0.1, attention_dropout=0.1
@@ -115,11 +111,10 @@ class AEGModel(AbstractG2MNet):
             hidden_dim=128, mlp_dim=512, dropout=0.1, attention_dropout=0.1
         )
         for ix in range(self.num_layers):
-            self.vit08.encoder.layers[ix].mlp[1] = OptAEGV3()
             self.vit16.encoder.layers[ix].mlp[1] = OptAEGV3()
             self.vit32.encoder.layers[ix].mlp[1] = OptAEGV3()
 
-        self.sln = SemiLinear(80 * 3, 80)
+        self.sln = SemiLinear(80 * 2, 80)
 
     def forward(self, glyph):
         xslice = IX.to(glyph.device) * th.ones_like(glyph)
