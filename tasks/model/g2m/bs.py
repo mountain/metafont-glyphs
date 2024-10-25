67572d0ea085b79d294134858a062fe6317afe77
Mingli Yuan <mingli.yuan@gmail.com>
2024-10-25 11:58:53 +0800

    typo

--- tasks/model/g2m/bs.py
+++ tasks/model/g2m/bs.py
@@ -43,7 +43,7 @@ class AbstractG2MNet(ltn.LightningModule):
         glyphs, labels = train_batch
         labels = labels.reshape(-1, 80)
         glyphs = glyphs.reshape(-1, 1, 96, 96)
-        logits = self.forward(glyphs, labels=labels).reshape(-1, 80)  # 传入 labels
+        logits = self.forward(glyphs, labels=labels)
         lss = self.loss(logits, labels)
         self.log('train_loss', lss, prog_bar=True)
         return lss
@@ -52,7 +52,7 @@ class AbstractG2MNet(ltn.LightningModule):
         glyphs, labels = val_batch
         labels = labels.reshape(-1, 80)
         glyphs = glyphs.reshape(-1, 1, 96, 96)
-        logits = self.forward(glyphs).reshape(-1, 80)  # 传入 labels
+        logits = self.forward(glyphs)
         lss = self.loss(logits, labels)
         self.log('val_loss', lss, prog_bar=True)
         return lss
@@ -61,7 +61,7 @@ class AbstractG2MNet(ltn.LightningModule):
         glyphs, labels = test_batch
         labels = labels.reshape(-1, 80)
         glyphs = glyphs.reshape(-1, 1, 96, 96)
-        logits = self.forward(glyphs).reshape(-1, 80)
+        logits = self.forward(glyphs)
         lss = self.loss(logits, labels)
         self.log('test_loss', lss)
         return lss
